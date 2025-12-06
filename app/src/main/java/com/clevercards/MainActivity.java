d5617d16f7ef93c09a15c187690911d761399599
Ashley <awozow@miracosta.edu>
2025-12-06 15:05:56 -0800

    Added the view binding dependency and added a toast for the sign out button on click action

--- app/src/main/java/com/clevercards/MainActivity.java
+++ app/src/main/java/com/clevercards/MainActivity.java
@@ -1,24 +1,33 @@
 package com.clevercards;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.clevercards.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //TODO: Wired up sign out functionality
+        // This should route the user back to the login page
+        binding.btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Signed Out!", Toast.LENGTH_SHORT).show();
+            }
         });
+
     }
 }
