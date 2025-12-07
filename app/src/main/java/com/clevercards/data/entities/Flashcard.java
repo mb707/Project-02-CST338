ec77c8c6173a568941046720a6eaadc915ee947c
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-06 22:38:47 -0800

    course entity done

--- app/src/main/java/com/clevercards/data/entities/Flashcard.java
+++ app/src/main/java/com/clevercards/data/entities/Flashcard.java
@@ -1,4 +1,8 @@
 package com.clevercards.data.entities;
-
+/**
+ * Name: Morgan Beebe
+ * Date: 2025-12-06
+ * Explanation: What is this class?
+ */
 public class Flashcard {
 }
--- app/src/main/java/com/clevercards/data/entities/User.java
+++ app/src/main/java/com/clevercards/data/entities/User.java
@@ -19,4 +19,48 @@ public class User {
     private String password;
     private boolean isAdmin;
 
+
+    //constructor
+
+    public User (String username, String password, boolean isAdmin){
+        this.username=username;
+        this.password=password;
+        this.isAdmin=isAdmin;
+
+    }
+
+    //getters and setters
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
