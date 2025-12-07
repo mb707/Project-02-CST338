da3b3c5cca43932f98938fc6b090c8ab7e482985
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-07 00:41:07 -0800

    fixed bug in coursedao

--- app/src/main/java/com/clevercards/data/dao/CourseDao.java
+++ app/src/main/java/com/clevercards/data/dao/CourseDao.java
@@ -2,6 +2,7 @@ package com.clevercards.data.dao;
 
 import com.clevercards.data.entities.Course;
 
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -12,7 +13,7 @@ import java.util.List;
  * Date: 2025-12-06
  * Explanation: Interface for the course dao
  */
-
+@Dao
 public interface CourseDao {
     //add new course (only created by awesome admins)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -23,7 +24,7 @@ public interface CourseDao {
     List<Course> getAllCourses();
 
     //get courses from a specific admin (professor)
-    @Query("SELECT * FROM Course WHERE createdByUser = :userId")
+    @Query("SELECT * FROM Course WHERE createdByUserId = :userId")
     List<Course> getCoursesByUser(int userId);
 
     //get a specific course based on its ID
