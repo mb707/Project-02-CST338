4c086b8563d834a78d0b2cd3e737f5235fec65e9
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-07 00:37:25 -0800

    fixed bugs, finished basic dao, entities, and repository

--- app/src/main/java/com/clevercards/data/entities/Flashcard.java
+++ app/src/main/java/com/clevercards/data/entities/Flashcard.java
@@ -1,8 +1,65 @@
 package com.clevercards.data.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 /**
  * Name: Morgan Beebe
  * Date: 2025-12-06
- * Explanation: What is this class?
+ * Explanation: flashcard entity for room database.
+ * stores all my front and back text and course it belongs to.
  */
+
+@Entity
 public class Flashcard {
+    @PrimaryKey(autoGenerate = true)
+    private int flashcardId;
+
+    private int courseId; //fk to course
+    private String frontText;
+    private String backText;
+
+    //constructor
+
+    public Flashcard(int courseId, String frontText, String backText){
+        this.courseId=courseId;
+        this.frontText=frontText;
+        this.backText=backText;
+
+    }
+
+    //getters and setters
+
+
+    public int getFlashcardId() {
+        return flashcardId;
+    }
+
+    public void setFlashcardId(int flashcardId) {
+        this.flashcardId = flashcardId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getFrontText() {
+        return frontText;
+    }
+
+    public void setFrontText(String frontText) {
+        this.frontText = frontText;
+    }
+
+    public String getBackText() {
+        return backText;
+    }
+
+    public void setBackText(String backText) {
+        this.backText = backText;
+    }
 }
