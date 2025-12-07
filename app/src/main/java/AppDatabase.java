098bb442d55be5dc32df2fc9d8fbb1b7ae82b7a3
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-06 19:43:30 -0800

    skeleton ready

--- app/src/main/java/AppDatabase.java
+++ app/src/main/java/AppDatabase.java
@@ -1,9 +1,14 @@
 import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
 /**
  * Name: Morgan Beebe
  * Date: 2025-12-06
  * Explanation: This is my class for the database
  */
+
 public class AppDatabase {
     //DAO Getters
     public abstract UserDao userDao();
@@ -12,6 +17,15 @@ public class AppDatabase {
 
     //Single Instance
     public static AppDatabase getInstance(Context context){
-
+        if(INSTANCE == null){
+            synchronized (AppDatabase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(contaxt.getApplicationContext(), AppDatabase.class, "clevercards_db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
     }
 }
