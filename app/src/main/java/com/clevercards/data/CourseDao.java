99af15e1203b1ffcb8d2b3d742daaf3d4191ce73
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-06 20:05:44 -0800

    course daoskeleton

--- app/src/main/java/com/clevercards/data/CourseDao.java
+++ app/src/main/java/com/clevercards/data/CourseDao.java
@@ -1,4 +1,29 @@
 package com.clevercards.data;
 
+import com.clevercards.data.entities.Course;
+
+import java.util.List;
+
+/**
+ * Name: Morgan Beebe
+ * Date: 2025-12-06
+ * Explanation: Interface for the course dao
+ */
+
 public interface CourseDao {
+    //add new course (only created by awesome admins)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertCourse(Course course);
+
+    //get all the courses into the system (for students to use)
+    @Query("SELECT * FROM Course")
+    List<Course> getAllCourses();
+
+    //get courses from a specific admin (professor)
+    @Query("SELECT * FROM Course WHERE createdByUser = :userId")
+    List<Course> getCoursesByUser(int userId);
+
+    //get a specific course based on its ID
+    @Query("SELECT * FROM Course WHERE courseId = :courseId LIMIT 1")
+    Course getCourseById(int courseId);
 }
