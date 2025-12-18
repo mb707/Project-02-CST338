31ff0837db039bff999f0953ebe1d40e935e2dfb
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-17 22:55:02 -0800

    basic unit tests for entities: flashcard, course, and appdatabase

--- app/src/test/java/com/clevercards/AppDatabaseTest.java
+++ app/src/test/java/com/clevercards/AppDatabaseTest.java
@@ -1,8 +1,27 @@
 package com.clevercards;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.clevercards.database.CleverCardsDatabase;
+
+import org.junit.Test;
+
 /**
  * Name: Morgan Beebe
  * Date: 2025-12-07
  * Explanation: class for testing my database
  */
 public class AppDatabaseTest {
+    @Test
+    public void database_tableNames_areDefined() {
+        assertEquals("userTable", CleverCardsDatabase.USER_TABLE);
+        assertEquals("courseTable", CleverCardsDatabase.COURSE_TABLE);
+        assertEquals("flashcardTable", CleverCardsDatabase.FLASHCARD_TABLE);
+    }
+
+    @Test
+    public void database_executor_isInitialized() {
+        assertNotNull(CleverCardsDatabase.databaseWriteExecutor);
+    }
 }
--- app/src/test/java/com/clevercards/CourseEntityTest.java
+++ app/src/test/java/com/clevercards/CourseEntityTest.java
@@ -1,8 +1,38 @@
 package com.clevercards;
+
+import static org.junit.Assert.assertEquals;
+
+import com.clevercards.entities.Course;
+
+import org.junit.Test;
+
 /**
  * Name: Morgan Beebe
  * Date: 2025-12-13
  * Explanation: unit test for course class
  */
 public class CourseEntityTest {
+
+    @Test
+    public void constructor_setsFieldsCorrectly() {
+        Course course = new Course("Math", 10);
+
+        assertEquals("Math", course.getCourseName());
+        assertEquals(10, course.getNumberOfCards());
+    }
+
+    @Test
+    public void setters_updateFieldsCorrectly() {
+        Course course = new Course("Science", 5);
+
+        course.setCourseId(1);
+        course.setCourseName("History");
+        course.setNumberOfCards(20);
+        course.setUserId(42);
+
+        assertEquals(1, course.getCourseId());
+        assertEquals("History", course.getCourseName());
+        assertEquals(20, course.getNumberOfCards());
+        assertEquals(42, course.getUserId());
+    }
 }
