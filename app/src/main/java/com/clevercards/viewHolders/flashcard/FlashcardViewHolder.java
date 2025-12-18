b1bd71fec40ecafe6a9aa3460bb709b73794379d
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-17 21:35:43 -0800

    added comments to flashcard view holder

--- app/src/main/java/com/clevercards/viewHolders/flashcard/FlashcardViewHolder.java
+++ app/src/main/java/com/clevercards/viewHolders/flashcard/FlashcardViewHolder.java
@@ -11,35 +11,33 @@ import com.clevercards.entities.Flashcard;
 /**
  * Author: Morgan and Ashley
  * Created on: 12/6/2025
- * Description: Flashcard View Holder
+ * Description: Flashcard View Holder hold references to the front and back text
  */
-
 public class FlashcardViewHolder extends RecyclerView.ViewHolder {
-
+    // TextView for the front side of the flashcard
     private final TextView frontTextView;
+    // TextView for the back side of the flashcard
     private final TextView backTextView;
+    // Root card view used to detect clicks
     private final View cardView;
-
+    // Tracks which side of the card is currently visible
     private boolean showingFront = true;
-
+    // Constructor binds layout views
     public FlashcardViewHolder(@NonNull View itemView) {
         super(itemView);
-
         cardView = itemView.findViewById(R.id.flashcardCard);
         frontTextView = itemView.findViewById(R.id.frontTextView);
         backTextView = itemView.findViewById(R.id.backTextView);
     }
-
+    // Binds flashcard data and sets up flip behavior
     public void bind(Flashcard flashcard) {
         // RESET STATE (CRITICAL)
         showingFront = true;
-
         frontTextView.setText(flashcard.getFrontText());
         backTextView.setText(flashcard.getBackText());
-
         frontTextView.setVisibility(View.VISIBLE);
         backTextView.setVisibility(View.GONE);
-
+        // Flip card on click
         cardView.setOnClickListener(v -> {
             if (showingFront) {
                 frontTextView.setVisibility(View.GONE);
