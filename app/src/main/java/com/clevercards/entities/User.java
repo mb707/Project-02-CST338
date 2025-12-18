f7e43be02b8caa1e18f7dda572e2d7008f382a32
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-17 21:39:42 -0800

    added comments to entity : user

--- app/src/main/java/com/clevercards/entities/User.java
+++ app/src/main/java/com/clevercards/entities/User.java
@@ -11,70 +11,58 @@ import java.util.Objects;
  * Name: Morgan Beebe
  * Date: 2025-12-06
  * Explanation: user entity for the room database
+ * a single user and thier role
  */
 
 @Entity(tableName = CleverCardsDatabase.USER_TABLE)
 public class User {
-
+    // Primary key for the user table
     @PrimaryKey(autoGenerate = true)
     private int userId;
-
+    // Username used for login
     private String username;
     private String password;
     private boolean isAdmin;
-
-
-    //constructor
-
+    //constructor used when creating a new user
     public User (String username, String password, boolean isAdmin){
         this.username=username;
         this.password=password;
         this.isAdmin=isAdmin;
 
     }
-
     //getters and setters
-
-
     public int getUserId() {
         return userId;
     }
-
     public void setUserId(int userId) {
         this.userId = userId;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public boolean isAdmin() {
         return isAdmin;
     }
-
     public void setAdmin(boolean admin) {
         isAdmin = admin;
     }
-
+    // Compares users based on all fields
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof User)) return false;
         User user = (User) o;
         return userId == user.userId && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
     }
-
+    // Generates hash code for the user
     @Override
     public int hashCode() {
         return Objects.hash(userId, username, password, isAdmin);
