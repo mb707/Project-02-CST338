fec059601d2d79726819733680627d43d934aa1c
Morgan Beebe <112012511+mb707@users.noreply.github.com>
2025-12-17 21:31:53 -0800

    added comments to flashcard view model

--- app/src/main/java/com/clevercards/viewHolders/flashcard/FlashcardViewModel.java
+++ app/src/main/java/com/clevercards/viewHolders/flashcard/FlashcardViewModel.java
@@ -12,30 +12,37 @@ import java.util.List;
 /**
  * Author: Morgan and Ashley
  * Created on: 12/6/2025
- * Description: Flashcard View Model
+ * Description: Flashcard View Model provides course related data to the flashcard UI
+ * somewhat focused on course for the moment, will only support flashcard data in the future
  */
 public class FlashcardViewModel {
     //TODO: adapt for flashcard
 
+    //repo for accessing course and flashcard data from the database
     private CleverCardsRepository repository;
 
+    //constructs view model and initializes the repo instance
     public FlashcardViewModel(@NonNull Application application) {
         super();
         repository = CleverCardsRepository.getRepository(application);
     }
 
+    //retrives all courses associated with a specif user
     public LiveData<List<Course>> getAllCoursesByUserId(int userId) {
         return repository.getAllCoursesByUserIdLiveData(userId);
     }
 
+    //retrieves all courses in the system
     public LiveData<List<Course>> getAllCoursesLD() {
         return repository.getAllCoursesLD();
     }
 
+    //updates an existing course in the database
     public void update(Course course) {
         repository.updateCourse(course);
     }
 
+    //inserts a new course into the database
     public void insert(Course course) {
         repository.insertCourse(course);
     }
